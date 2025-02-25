5a1bcd3005c2abe0f5382c12ae8ec7fe7bfae42d
Maturana, Nicole <nicole.maturana@arkanosoft.com>
2025-02-25 10:06:43 -0300

    Life cycles

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChange } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
@@ -9,5 +10,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
 
+  constructor(){
+    //no async
+    //before render
+    console.log('constructor');
+    console.log('-', repeat(10));
+  }
+
+  ngOnChanges(change: SimpleChange){
+    //before and during render
+    console.log('ngOnChanges');
+    console.log('-', repeat(10));
+    console.log(change);
+  }
 }
