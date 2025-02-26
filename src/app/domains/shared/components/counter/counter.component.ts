bc5c2cf2f4d40f8e336e290391cd54d7def59ec8
Maturana, Nicole <nicole.maturana@arkanosoft.com>
2025-02-26 13:00:35 -0300

    Cap 35 to 40

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChange } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { repeat } from 'rxjs';
 
@@ -12,18 +12,61 @@ import { repeat } from 'rxjs';
 export class CounterComponent {
   @Input({required: true}) duration = 0;
   @Input({required: true}) message = '';
+  counter = signal(0);
+  counterRef: number | undefined;
 
   constructor(){
     //no async
     //before render
+    //one time
     console.log('constructor');
     console.log('-', repeat(10));
   }
 
-  ngOnChanges(change: SimpleChange){
+  ngOnChanges(changes: SimpleChanges){
     //before and during render
+    //many times
     console.log('ngOnChanges');
     console.log('-', repeat(10));
-    console.log(change);
+    console.log(changes);
+    const duration = changes['duration'];
+    console.log(duration);
+    if(duration && duration.currentValue !== duration.previousValue){
+      this.doSomething();
+    }
+  }
+
+  ngOnInit(){
+    //after render
+    //one time
+    //async, then, subs
+    console.log('ngOnInit');
+    console.log('-', repeat(10));
+    console.log('duration => ', this.duration);
+    console.log('message => ', this.message);
+    /**this.counterRef = window.setInterval(() => {
+      console.log('run interval');
+      this.counter.update(statePrev => statePrev + 1);
+    }, 1000);**/
+  }
+
+  ngAfterViewInit(){
+    //after render
+    //hijos ya fueron pintados
+    console.log('ngAfterViewInit');
+    console.log('-', repeat(10));
+    console.log('duration => ', this.duration);
+    console.log('message => ', this.message);
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+    console.log('-', repeat(10));
+
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething(){
+    console.log('change duration');
   }
 }
